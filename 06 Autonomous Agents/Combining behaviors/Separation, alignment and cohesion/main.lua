173c1d38d00821a2d9f40f446d686cff734fdaf6
borntofrappe <borntofrappe@gmail.com>
2021-01-22 14:18:02 +0100

    code | combining alignment, separation, cohesion

---
+++ 06 Autonomous Agents/Combining behaviors/Separation, alignment and cohesion/main.lua
@@ -0,0 +1,72 @@
+LVector = require "LVector"
+require "Vehicle"
+
+WINDOW_WIDTH = 600
+WINDOW_HEIGHT = 400
+SIZE_VEHICLE = 6
+MAX_SPEED = 20
+MAX_FORCE = 10
+UPDATE_SPEED = 8
+VELOCITY_MIN = 10
+VELOCITY_MAX = 30
+DISTANCE_VEHICLE_ALIGNMENT = 80
+DISTANCE_VEHICLE_COHESION = 50
+DISTANCE_VEHICLE_SEPARATION = 20
+VEHICLES = 7
+ALIGNMENT_MULTIPLIER = 2
+COHESION_MULTIPLIER = 1
+SEPARATION_MULTIPLIER = 2
+
+function love.load()
+  love.window.setTitle("Autonomous Agents - Combining behaviors - Separation, alignment and cohesion")
+  love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT)
+  love.graphics.setBackgroundColor(1, 1, 1)
+
+  math.randomseed(os.time())
+
+  vehicles = {}
+  for i = 1, VEHICLES do
+    table.insert(vehicles, Vehicle:new())
+  end
+end
+
+function love.mousepressed(x, y, button)
+  if button == 2 then
+    vehicles = {}
+    for i = 1, VEHICLES do
+      table.insert(vehicles, Vehicle:new())
+    end
+  end
+end
+
+function love.update(dt)
+  for i, vehicle in ipairs(vehicles) do
+    vehicle:update(dt)
+
+    vehicle:applyBehaviors(vehicles)
+
+    if vehicle.position.x < -vehicle.size then
+      vehicle.position.x = WINDOW_WIDTH + vehicle.size
+    elseif vehicle.position.x > WINDOW_WIDTH + vehicle.size then
+      vehicle.position.x = -vehicle.size
+    end
+
+    if vehicle.position.y < -vehicle.size then
+      vehicle.position.y = WINDOW_HEIGHT + vehicle.size
+    elseif vehicle.position.y > WINDOW_HEIGHT + vehicle.size then
+      vehicle.position.y = -vehicle.size
+    end
+  end
+
+  if love.mouse.isDown(1) then
+    local x, y = love.mouse:getPosition()
+    local position = LVector:new(x, y)
+    table.insert(vehicles, Vehicle:new(position))
+  end
+end
+
+function love.draw()
+  for i, vehicle in ipairs(vehicles) do
+    vehicle:render()
+  end
+end
